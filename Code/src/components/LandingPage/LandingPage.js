193d644ba53d06a13c5a946a7b96627ebbcb7e53
Alexander Au <alex@eduroam-169-233-212-215.ucsc.edu>
2018-02-05 14:03:44 -0800

    Got bitcoin api call working

--- Code/src/components/LandingPage/LandingPage.js
+++ Code/src/components/LandingPage/LandingPage.js
@@ -10,12 +10,22 @@ class LandingPage extends React.Component {
     constructor(props){
       super(props)
       this.state = {
-        url: "http://localhost:3001/users/"
+        url: "http://localhost:3001/users/",
+        coin: '',
+        price: ''
       }
+       this.handleClick = this.handleClick.bind(this)
     }
 
     componentDidMount(){
       console.log("Landing Page mounted")
+    }
+
+    handleClick () {
+      console.log('Button pressed')
+      axios.get('https://api.coinmarketcap.com/v1/ticker/ripple/')
+        .then(response=>this.setState({coin: response.data[0].name, price: response.data[0].price_usd}))
+
     }
 
     render(){
@@ -23,47 +33,31 @@ class LandingPage extends React.Component {
         <div className={classes.landingPage}>
           <header className={classes.header}>
             <h2 className={classes.title}>
-              onCareer
+              CryptoTracker
             </h2>
 
             <nav className={classes.navigation}>
-              <button className={classes.nav_button}>
-                About
+              <button className={classes.nav_button} onClick={this.handleClick}>
+                Check price (this is a test pls no make fun of design)
               </button>
-
               <button className={classes.nav_button}>
                 Sign In
               </button>
             </nav>
           </header>
-
           <p className={classes.description}>
-            Organize and keep track of your job applications in an efficient manner
+            {this.state.coin}, {this.state.price}
           </p>
-
           <SignIn />
 
-          <img className={classes.heroimage} src={screenshot} alt="screenshot" />
-          
+
           <div className={classes.about}>
-            <h3 className={classes.about_title}>
-              About
-            </h3>
-            <p className={classes.about_description}>
-              This is a project done by a group of students from University of California, Santa Cruz.
-              <br/>
-              <br/>
-              The web application is built on MERN stack (MongoDB + Express + React + Node.js).
-              <br/>
-              And it consists of three core features: Dashboard, Calendar, and Analytics.
-              <br/>
-              <br/>
-              If you have any questions or comments, feel free to reach out to us at <a href="mailto:oncareer115@gmail.com" target="_top">onCareer115@gmail.com</a>.
-            </p>
+
             <p className={classes.copyright}>
               Copyright 2018
             </p>
           </div>
+        
         </div>
         )
     }
