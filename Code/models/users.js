cf228de52c687b4186ef51fec9ae015c8009db7d
dcalta <drewcaltagirone@gmail.com>
2018-02-20 20:01:44 -0800

    More code set up please

--- Code/models/users.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var UsersSchema = new mongoose.Schema({
-  user_name: String,
-  user_email: String,
-  user_id: String
-})
-
-module.exports = mongoose.model('User', UsersSchema)
