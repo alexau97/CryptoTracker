6d6e1f8d3cec52e5ba5ce0d0a6671feba0d8305b
Alexander Au <alex@Alexanders-MacBook-Pro.local>
2018-02-22 17:05:22 -0800

    set up api calls for all cryptocurrencies

--- sourcecode/src/components/Home/Home.js
+++ sourcecode/src/components/Home/Home.js
@@ -1,42 +1,89 @@
 import React, { Component } from 'react';
-import { Route, Switch, Link, withRouter } from 'react-router-dom';
 import classes from './Home.css';
-import history from '../../history';
+import SignIn from '../Login/Login';
+import Auth from '../Login/Auth';
+import axios from 'axios';
 
-
-function toAccount() {
-  history.push('/account');
-  window.location.reload();
-}
+//this.state.coin = ['Bitcoin', 'Ethereum', 'Ripple', ...]
+//this.state.price = ['10,000.05', '942.55', '1.45', ...]
 
 class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			url: "http://localhost:3001/users/",
+			coin: [],
+        	price: []
+		}
+		//this.chooseAPICall = this.chooseAPICall.bind(this)
+	}
 
-  render() {
+	componentDidMount(){
+		console.log("Home Page mounted");
+		axios.get('https://api.coinmarketcap.com/v1/ticker/')
+			.then(response=>console.log(response.data))
+		this.getPrices();
+	}
 
-    return (
-        <div className={classes.landingPage}>
+	getPrices(){
+		var currentCoin = this.state.coin;
+		var currentPrice = '';
+		for (var i = 0; i<10; i++){
+			console.log("index is = "+ i);
+			axios.get('https://api.coinmarketcap.com/v1/ticker/')
+				.then(response=>this.setState({
+					coin: this.state.coin.concat(response.data[i].name),
+					price: this.state.price.concat(response.data[i].price_usd)
+			}))
+			// console.log(this.state.coin[i])
+			// console.log(this.state.price[i])
+		}
+	}
+
+	// chooseAPICall(cryptoName){
+	// 	console.log(cryptoName+'has been chosen');
+	// 	var apiCallURL = 'https://api.coinmarketcap.com/v1/ticker/'+cryptoName + '/';
+	// 	console.log("call url" + apiCallURL);
+	// 	axios.get(String(apiCallURL))
+	// 		.then(response=>this.setState({
+	// 			coin: response.data[0].name,
+	// 			price: response.data[0].price_usd
+	// 		}))
+	// }
+
+
+   render(){
+        return (
+        <div className={classes.home}>
           <header className={classes.header}>
             <h2 className={classes.title}>
-              CryptoTracker
+              Home Page
             </h2>
-
-            <nav className={classes.navigation}>
-              <button className={classes.nav_button}>
-                Home
-              </button>
-
-              <button onClick={toAccount} className={classes.nav_button}>
-
-                Account
-              </button>
-            </nav>
-
           </header>
-          
+          <p className={classes.description}>
+            {this.state.coin[5]}, {this.state.price[5]}
+          </p>
+          <SignIn />
+
+
+          <div className={classes.about}>
+            <h3 className={classes.about_title}>
+              About
+            </h3>
+            <p className={classes.about_description}>
+              This is a project done by a group of students from University of California, Santa Cruz. This is a web application that is created using JavaScript, React, and Google Firebase. 
+              The purpose for this web application is to
+            </p>
+            <p className={classes.copyright}>
+              Copyright 2018
+            </p>
+          </div>
+        
         </div>
-    );
-  }
+        )
+    }
 
 }
+
 
 export default Home;
