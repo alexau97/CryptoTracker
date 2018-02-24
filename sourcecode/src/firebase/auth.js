e727f23aa1cc507b6b148b1b3ae7788acddffbfe
ablimm <ablim@ucsc.edu>
2018-02-24 14:40:46 -0800

    setup Firebase's Authentication API

---
+++ sourcecode/src/firebase/auth.js
@@ -0,0 +1,9 @@
+import {auth} from './firebase';
+
+// Sign Up
+export const createUserWithEmailAndPassword = (email, password) =>
+  auth.createUserWithEmailAndPassword(email, password);
+
+ // Sign In
+export const signInWithEmailAndPassword = (email, password) =>
+  auth.signInWithEmailAndPassword(email, password);
---
+++ sourcecode/src/firebase/index.js
@@ -0,0 +1,9 @@
+import * as auth from './auth';
+import * as firebase from './firebase';
+
+//middle man between user and auth/firebase files.
+export {
+  auth,
+  firebase,
+};
+
